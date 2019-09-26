7ae2e973312b28a42e3a1dd9ff5e3c61ef4a7df2
Zsolt M <hulfyback@gmail.com>
2019-09-26 20:12:59 +0200

    refactor(ms-homework): use isinstance() to check type
    use isinstance method in OrderBook class to check whether the attributes are correct types

--- data_structures.py
+++ data_structures.py
@@ -4,27 +4,15 @@ class Quote:
 
     def __init__(self, quantity, price):
         try:
-<<<<<<< HEAD
-            if quantity.__class__() != 0:
-=======
             if isinstance(quantity, int):
                 self.quantity = quantity
             else:
->>>>>>> mzs-subtask-a
                 raise errors.NotAnIntegerError
-            else:
-                self.quantity = quantity
 
-<<<<<<< HEAD
-            if price.__class__() != 0 or price.__class__() != 0.0:
-=======
             if isinstance(price, float) or isinstance(price, int):
                 self.price = price
             else:
->>>>>>> mzs-subtask-a
                 raise errors.NotANumberError
-            else:
-                self.price = price
 
             self.exchange = ''
             
@@ -40,26 +28,26 @@ class OrderBook:
 
     def __init__(self, name):
         try:
-            if name.__class__() != '':
-                raise TypeError
-            else: 
+            if isinstance(name, str):
                 self.name = name
                 self.quotes = []
+            else: 
+                raise TypeError
         except TypeError:
             print('Error: The type of the name must be a string')
 
     def __repr__(self):
         order_book_repr = ''
         for quote in self.quotes:
-            order_book_repr += f'{quote.__repr__()} | '
+            order_book_repr += f'{quote} | '
         return order_book_repr
 
     def add_quote(self, quote):
         try:
-            if len(str(quote.__class__).split('.')) == 1 or str(quote.__class__).split('.')[1].split("'")[0] != 'Quote':
-                raise TypeError
-            else:
+            if isinstance(quote, Quote):
                 quote.exchange = self.name
                 self.quotes.append(quote)
+            else:
+                raise TypeError
         except TypeError:
             print('Error: Type of the argument must be Quote')
