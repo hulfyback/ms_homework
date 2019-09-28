db6f30ff793e9f950cf04531ca9d3f3cb53b712a
Zsolt M <hulfyback@gmail.com>
2019-09-28 10:32:41 +0200

    feat(ms-homework): make Quote comparable
    override __eg__(), __ge__(), __le()__, __gt__() and __lt__() methods

--- data_structures.py
+++ data_structures.py
@@ -1,18 +1,16 @@
-import errors
-
-
 class Quote:
-
-    def __init__(self, quantity, price):
+    price= None
+    
+    def __init__(self, quantity=0, price=0):
         try:
             if isinstance(quantity, int):
-                if quantity > 0:
+                if quantity >= 0:
                     self.quantity = quantity
                 else:
                     raise errors.NegativeNumberError
     
                 if isinstance(price, float) or isinstance(price, int):
-                    if price > 0:
+                    if price >= 0:
                         self.price = price
                         self.exchange = ''
                     else:
@@ -23,7 +21,7 @@ class Quote:
                 raise errors.NotANumberError
             
         except errors.NotAnIntegerError:
-            print('Error: The type of the quentity must be integer')
+            print('Error: The type of the quantity must be integer')
         except errors.NotANumberError:
             print('Error: The type of the price must be number')
         except errors.NegativeNumberError:
@@ -32,10 +30,9 @@ class Quote:
     def __add__(self, other_quote):
         try:
             if isinstance(other_quote, Quote):
-                if self.price == other_quote.price:
+                if self.price == other_quote.price or min(self.price, other_quote.price) == 0:
                     total_qantity = self.quantity + other_quote.quantity
-                    price = self.price
-                    return Quote(total_qantity, price)
+                    return Quote(total_qantity, max(self.price, other_quote.price))
                 else:
                     raise ValueError
             else:
@@ -46,7 +43,55 @@ class Quote:
             print('Error: The price of the quotes must be equals')
 
     def __radd__(self, other_quote):
-        return self.__add__(other_quote)
+        if other_quote == 0:
+            return self
+        else:
+            return self.__add__(other_quote)
 
     def __str__(self):
         return f'{self.quantity}@{self.price}'
+
+    def __eq__(self, other_quote):
+        try:
+            if isinstance(other_quote, Quote) != True:
+                raise TypeError
+            else:
+                return not self.price < other_quote.price and not other_quote.price < self.price
+        except TypeError:
+            print('Error: The type of the argument must be Quote')
+
+    def __ge__(self, other_quote):
+        try:
+            if isinstance(other_quote, Quote) != True:
+                raise TypeError
+            else:
+                return not self.price < other_quote.price
+        except TypeError:
+            print('Error: The type of the argument must be Quote')
+
+    def __le__(self, other_quote):
+        try:
+            if isinstance(other_quote, Quote) != True:
+                raise TypeError
+            else:
+                return not other_quote.price < self.price
+        except TypeError:
+            print('Error: The type of the argument must be Quote')
+
+    def __gt__(self, other_quote):
+        try:
+            if isinstance(other_quote, Quote) != True:
+                raise TypeError
+            else:
+                return other_quote.price < self.price
+        except TypeError:
+            print('Error: The type of the argument must be Quote')
+
+    def __lt__(self, other_quote):
+        try:
+            if isinstance(other_quote, Quote) != True:
+                raise TypeError
+            else:
+                return self.price < other_quote.price
+        except TypeError:
+            print('Error: The type of the argument must be Quote')
