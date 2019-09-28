6bdbc123d030add1dcd197999a2449927c489249
Zsolt M <hulfyback@gmail.com>
2019-09-28 18:31:23 +0200

    refactor(ms-homework): add decorators
    define decorator functions in errors.py for handling errors to make clearer and more readable the code

--- data_structures.py
+++ data_structures.py
@@ -1,97 +1,88 @@
 import errors
+import numbers
 
 
 class Quote:
-    price= None
+    price= 0
     
+    @errors.catch_not_an_int_error
+    @errors.catch_not_a_number_error
+    @errors.catch_negative_number_error
     def __init__(self, quantity=0, price=0):
-        try:
-            if isinstance(quantity, int) is False:
-                raise errors.NotAnIntegerError
-            elif isinstance(price, float) is False or isinstance(price, int) is False:
-                raise errors.NotANumberError
-            elif quantity < 0 or price < 0:
-                raise errors.NegativeNumberError
-            else:
-                self.quantity = quantity        
-                self.price = price
-                self.exchange = ''            
-        except errors.NotAnIntegerError:
-            print(errors.NotANumberError.error_msg)
-        except errors.NotANumberError:
-            print(errors.NotANumberError.error_msg)
-        except errors.NegativeNumberError:
-            print(errors.NegativeNumberError.error_msg)
+        if isinstance(quantity, int) is False:
+            raise errors.NotAnIntegerError
+        elif isinstance(price, numbers.Number)is False:
+            raise errors.NotANumberError
+        elif quantity < 0 or price < 0:
+            raise errors.NegativeNumberError
+        else:
+            self.quantity = quantity        
+            self.price = price
+            self.exchange = ''            
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
+    @errors.catch_value_error
     def __add__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) is False:
-                raise TypeError
-            elif self.price != other_quote.price or min(self.price, other_quote.price) != 0:
-                raise ValueError
-            else:
-                total_qantity = self.quantity + other_quote.quantity
-                return Quote(total_qantity, max(self.price, other_quote.price))
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
-        except ValueError:
-            print('Error: The price of the quotes must be equals')
+        if isinstance(other_quote, Quote) is False:
+            raise TypeError
+        elif self.price != other_quote.price or min(self.price, other_quote.price) != 0:
+            raise ValueError
+        else:
+            total_qantity = self.quantity + other_quote.quantity
+            return Quote(total_qantity, max(self.price, other_quote.price))
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __radd__(self, other_quote):
-        try:
-            if other_quote == 0:
-                return self
-            elif isinstance(other_quote, Quote) is False:
-                raise TypeError
-            else:
-                return self.__add__(other_quote)
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if other_quote == 0:
+            return self
+        elif isinstance(other_quote, Quote) is False:
+            raise TypeError
+        else:
+            return self.__add__(other_quote)
 
     def __str__(self):
         return f'{self.quantity}@{self.price}'
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __eq__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) != True:
-                raise TypeError
-            else:
-                return not self.price < other_quote.price and not other_quote.price < self.price
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if isinstance(other_quote, Quote) != True:
+            raise TypeError
+        else:
+            return not self.price < other_quote.price and not other_quote.price < self.price
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __ge__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) != True:
-                raise TypeError
-            else:
-                return not self.price < other_quote.price
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if isinstance(other_quote, Quote) != True:
+            raise TypeError
+        else:
+            return not self.price < other_quote.price
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __le__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) != True:
-                raise TypeError
-            else:
-                return not other_quote.price < self.price
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if isinstance(other_quote, Quote) != True:
+            raise TypeError
+        else:
+            return not other_quote.price < self.price
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __gt__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) != True:
-                raise TypeError
-            else:
-                return other_quote.price < self.price
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if isinstance(other_quote, Quote) != True:
+            raise TypeError
+        else:
+            return other_quote.price < self.price
 
+    @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def __lt__(self, other_quote):
-        try:
-            if isinstance(other_quote, Quote) != True:
-                raise TypeError
-            else:
-                return self.price < other_quote.price
-        except TypeError:
-            print(errors.ErrorMessages.QUOTE)
+        if isinstance(other_quote, Quote) != True:
+            raise TypeError
+        else:
+            return self.price < other_quote.price
+
+q1 = Quote(100, 0.1)
+q2 = Quote(200, 0.1)
+q3 = Quote(-1, 0.3)
+q4 = Quote(23.2, 1)
+
+print(q1 == q2)
+print(q1 == 'Quote')
+print(q1 + (3, 5))
--- errors.py
+++ errors.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class ErrorMessages(Enum):
+    def __str__(self):
+        return str(self.value)
     msg = 'Error: The type of the argument must be '
     QUOTE = f'{msg} Quote'
 
@@ -22,3 +24,45 @@ class NegativeNumberError(Error):
     """Raised when the input value is lower then zero"""
     error_msg = 'Error: The value of the argument must be greater then 0'
     pass
+
+def catch_type_error(postfix):
+    def outer_wrapper_func(func):
+        def wrapper_func(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except TypeError:
+                print(ErrorMessages.QUOTE)
+        return wrapper_func
+    return outer_wrapper_func
+
+def catch_value_error(func):
+    def wrapper_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            print('Error: The price of the quotes must be equals')
+    return wrapper_func
+
+def catch_not_an_int_error(func):
+    def wrapper_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except NotAnIntegerError:
+            print(NotAnIntegerError.error_msg)
+    return wrapper_func
+
+def catch_not_a_number_error(func):
+    def wrapper_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except NotANumberError:
+            print(NotANumberError.error_msg)
+    return wrapper_func
+
+def catch_negative_number_error(func):
+    def wrapper_func(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except NegativeNumberError:
+            print(NegativeNumberError.error_msg)
+    return wrapper_func
