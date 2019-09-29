183ffee8d2bfa98a306fa1da18bad12a5fbe4546
Zsolt M <hulfyback@gmail.com>
2019-09-29 17:27:27 +0200

    doc(ms-homework): add docstring to OrderBook

--- order_book.py
+++ order_book.py
@@ -3,6 +3,12 @@ from quote import Quote
 
 
 class OrderBook:
+    '''
+    Datastructure to represent an order book.
+
+    Attributes:
+        name (str): Name of the order book.
+    '''
     
     @errors.catch_type_error(errors.ErrorMessages.STRING)
     def __init__(self, name):
@@ -13,6 +19,15 @@ class OrderBook:
             raise TypeError
 
     def __str__(self):
+        '''
+        Represents the order book in a string.
+
+        Args:
+            None.
+        
+        Returns:
+            A string e.g.: 100@0.1 | 200@0.25
+        '''
         order_book_str = ''
         i_quotes = iter(self.quotes)
         try:
@@ -27,13 +42,38 @@ class OrderBook:
 
     @errors.catch_type_error(errors.ErrorMessages.QUOTE)
     def add_quote(self, quote):
+        '''
+        Add a quote to the order book.
+
+        Args:
+            quote: Quote to be added to the order book.
+
+        Returns:
+            None. The quote will be added to the original order book.
+
+        Raises:
+            TypeError: An error occured if the argument is not a Quote.
+        '''
         if isinstance(quote, Quote):
             quote.exchange = self.name
             self.quotes.append(quote)
         else:
             raise TypeError
+
     @errors.catch_type_error(errors.ErrorMessages.LIST)
     def add_quotes(self, quotes):
+        '''
+        Add a list of quotes to the order book.
+
+        Args:
+            quotes (list): List of quotes to be added.
+
+        Returns:
+            None. The quotes will be added to the original order book.
+
+        Raises:
+            TypeError: An error occured if the argument is not a list of Quotes"
+        '''
         if isinstance(quotes, list) is False:
             raise TypeError
         else:
